f75b9e33282fc0c6680a3ff55e35791a16053bc6
iahumadaz <i.ahumada@duocuc.cl>
2023-05-06 15:40:37 -0400

    cambs

--- js/validador.js
+++ js/validador.js
@@ -52,46 +52,9 @@
 */
 
 $(document).ready(function() {
-
-  $.validator.addMethod("rutChileno", function(value, element) {
-    // Eliminar puntos y guión del RUT
-    value = value.replace(/[.-]/g, "");
-
-    // Validar que el RUT tenga 8 o 9 dígitos
-    if (value.length < 8 || value.length > 9) {
-      return false;
-    }
-
-    // Validar que el último dígito sea un número o una 'K'
-    var validChars = "0123456789K";
-    var lastChar = value.charAt(value.length - 1).toUpperCase();
-    if (validChars.indexOf(lastChar) == -1) {
-      return false;
-    }
-
-    // Calcular el dígito verificador
-    var rut = parseInt(value.slice(0, -1), 10);
-    var factor = 2;
-    var sum = 0;
-    var digit;
-    while (rut > 0) {
-      digit = rut % 10;
-      sum += digit * factor;
-      rut = Math.floor(rut / 10);
-      factor = factor === 7 ? 2 : factor + 1;
-    }
-    var dv = 11 - (sum % 11);
-    dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
-
-    // Validar que el dígito verificador sea correcto
-    return dv === lastChar;
-  }, "Por favor ingrese un RUT válido."); 
-  $("#formulario1").validate({
+ 
+  $("#formularioIngreso").validate({
     rules: {
-      rut: {
-        required: true,
-        rutChileno: true
-      },
       email: {
         required: true,
         email: true,
@@ -99,6 +62,7 @@ $(document).ready(function() {
       password: {
         required: true,
         minlength: 5,
+        maxlength: 30,
       },
       password2: {
         required: true,
@@ -106,10 +70,6 @@ $(document).ready(function() {
       },
     }, // --> Fin de reglas
     messages: {
-      rut: {
-        required: "El rut es un campo obligatorio",
-        rutChileno: "El formato del rut no es válido"
-      },
       email: {
         required: "El email es un campo requerido",
         email: "El email no cumple el formato de un correo",
@@ -117,6 +77,7 @@ $(document).ready(function() {
       password: {
         required: "La contraseña es una campo obligatorio",
         minlength: "Mínimo 5 caracteres",
+        maxlength: "Máximo 30 caracteres",
       },
       password2: {
         required: "Repita la contraseña anterior",
